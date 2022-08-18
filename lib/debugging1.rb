52d3cf09c02d9a8a2ed8fd06294ebd22380ec598
Jonas Diete <jonas.diete@gmail.com>
2022-08-18 11:52:21 +0100

    finished debugging exercises

--- lib/debugging1.rb
+++ lib/debugging1.rb
@@ -1,11 +1,7 @@
 def encode(plaintext, key)
-  cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
-  puts "The cipher is #{cipher}"
+  cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
   ciphertext_chars = plaintext.chars.map do |char|
-      "This char of the plaintext is #{char}"
-      "The index of this char in the cipher is #{cipher.find_index(char)}"
-      "Added 65 and turned into a chr it is #{(65 + cipher.find_index(char)).chr}"
-      (65 + cipher.find_index(char)).chr
+    (65 + cipher.find_index(char)).chr
   end
   return ciphertext_chars.join
 end
@@ -17,7 +13,3 @@ def decode(ciphertext, key)
   end
   return plaintext_chars.join
 end
-
-puts encode("theswiftfoxjumpedoverthelazydog", "secretkey")
-
-puts decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
